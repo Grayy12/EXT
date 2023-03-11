94f2b76aeeb7f1f6c6fdbfcb2d2744c9cf0fa055
Grayy12 <grayy161@gmail.com>
2023-03-10 23:07:01 -0500

    Create Player.lua

---
+++ Player.lua
@@ -0,0 +1,45 @@
+local cons = loadstring(game:HttpGet("https://raw.githubusercontent.com/Grayy12/EXT/main/connections.lua", true))()
+
+local Player
+Player = {
+	plr = game.Players.LocalPlayer
+		or game.Players:GetPropertyChangedSignal("LocalPlayer"):Wait() and game.Players.LocalPlayer,
+
+	char = function()
+		return game.Players.LocalPlayer.Character
+			or game.Players.LocalPlayer.CharacterAdded:Wait() and game.Players.LocalPlayer.Character
+	end,
+
+	root = function()
+		return Player.c().HumanoidRootPart or Player.c().PrimaryPart
+	end,
+
+	hum = function()
+		return Player.c():WaitForChild("Humanoid")
+	end,
+
+	_rescons = {},
+
+	respawn = function(func)
+		print("Res")
+		if func and type(func) == "function" then
+			if not table.find(Player._rescons, func) then
+				table.insert(Player._rescons, func)
+			end
+		end
+
+		for _, v in next, Player._rescons do
+			local s, n = pcall(function()
+				coroutine.wrap(v)()
+			end)
+
+			if not s then
+				warn(n)
+			end
+		end
+	end,
+}
+
+cons.new(Player.plr.CharacterAdded:Connect(Player.respawn))
+
+return Player
