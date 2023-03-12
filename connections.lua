490e3f204413d09f15fe1b76ead8f9e69e8ba916
Grayy12 <grayy161@gmail.com>
2023-03-11 21:04:42 -0500

    Update connections.lua

--- connections.lua
+++ connections.lua
@@ -1,16 +1,44 @@
 local cons = {}
+cons.__index = cons
+
 local g = getgenv()
 
 if g._connections then
 	for i, v in next, g._connections do
-		v:Disconnect()
+		if v.Connected then
+			v:Disconnect()
+		end
+		v = nil
 	end
 else
 	g._connections = {}
 end
 
-function cons.new(connection: RBXScriptConnection)
-	table.insert(g._connections, connection)
+function cons.new(signal: RBXScriptSignal, func)
+	local self = setmetatable({}, cons)
+	self.func = typeof(func) == "function" and func or function() end
+	self.signal = signal
+	self.conection = signal:Connect(self.func)
+
+	table.insert(g._connections, self.conection)
+
+	function self:disable()
+		self.conection:Disconnect()
+	end
+
+	function self:enable()
+		table.remove(g._connections, table.find(g._connections, self.conection))
+		self.conection = self.signal:Connect(self.func)
+		table.insert(g._connections, self.conection)
+	end
+
+	function self:delete()
+		self.conection:Disconnect()
+
+		table.remove(g._connections, table.find(g._connections, self.conection))
+	end
+
+	return self
 end
 
 return cons
