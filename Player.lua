a26a21a869fe1ecf9026c169dd2a3da9da01594c
Grayy12 <grayy161@gmail.com>
2024-04-23 21:23:01 -0400

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -1,14 +1,12 @@
 local ConnectionHandlerModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Grayy12/EXT/main/connections.lua", true))()
-local connectionManager = ConnectionHandlerModule.new('PlayerModuleGrayy')
+local connectionManager = ConnectionHandlerModule.new("PlayerModuleGrayy")
 
 local Player
 Player = {
-	plr = game.Players.LocalPlayer
-		or game.Players:GetPropertyChangedSignal("LocalPlayer"):Wait() and game.Players.LocalPlayer,
+	plr = game.Players.LocalPlayer or game.Players:GetPropertyChangedSignal("LocalPlayer"):Wait() and game.Players.LocalPlayer,
 
 	char = function()
-		return game.Players.LocalPlayer.Character
-			or game.Players.LocalPlayer.CharacterAdded:Wait() and game.Players.LocalPlayer.Character
+		return game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
 	end,
 
 	root = function()
@@ -22,34 +20,34 @@ Player = {
 	_rescons = {},
 
 	respawn = function(func)
-		local function callFunctions()
-			for _, v in next, Player._rescons do
-				local s, n = pcall(function()
-					coroutine.wrap(v)()
-				end)
-
-				if not s then
-					warn(n)
-				end
+		local function runFunctions()
+			for _, v in pairs(Player._rescons) do
+				coroutine.wrap(v)()
 			end
 		end
-		if typeof(func) ~= "function" then
-			callFunctions()
+
+		if type(func) ~= "function" then
+			runFunctions()
+			return
 		end
 
-		if func and type(func) == "function" then
-			if not table.find(Player._rescons, func) then
-				table.insert(Player._rescons, func)
-				local res = {}
-				res.__index = res
-				res.idx = table.find(Player._rescons, func)
-				res.func = func
-				function res:delete()
-					table.remove(Player._rescons, self.idx)
-					callFunctions()
-				end
-				return res
-			end
+		if not table.find(Player._rescons, func) then
+			table.insert(Player._rescons, func)
+
+			return setmetatable({
+				idx = #Player._rescons,
+				func = func,
+			}, {
+				__call = function(self)
+					self.func()
+				end,
+
+				__index = {
+					delete = function(self)
+						table.remove(Player._rescons, self.idx)
+					end,
+				},
+			})
 		end
 	end,
 }
